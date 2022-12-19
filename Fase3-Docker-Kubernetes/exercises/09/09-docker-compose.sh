77c1f5a3a2eebe995bc65705a43af84fea487002
erickarroyo1 <earroyo@lulobank.com>
2022-12-19 14:37:17 -0500

    bootcamp/excercises/09-v1

--- Fase3-Docker-Kubernetes/exercises/09/09-docker-compose.sh
+++ Fase3-Docker-Kubernetes/exercises/09/09-docker-compose.sh
@@ -2,7 +2,7 @@
 
 echo -e '\n\nConstruyendo la imàgen PokeApi basada en kas definiciones del dockerfile usando Python Base\n\n'
 
-docker-compose build --tag=pokeapi:0.0.1 .
+docker build -t pokeapi:0.0.1 .
 
 
 echo -e '\n\nPush de la imágen a dockerhub\n\n'
@@ -11,10 +11,9 @@ docker login
 docker tag pokeapi:0.0.1 191006/pokeapi:0.0.1
 docker push 191006/pokeapi:0.0.1
 
-echo -e '\n\nDocker Run para inicializar el contenedor\n\n'
+echo -e '\n\ndocker-compose up para inicializar el contenedor\n\n'
 
-docker-compose up 
-
+docker-compose up -d
 
 echo -e '\n\nTesting app pokeapi, pto tcp 5000, en 10 segundos \n\n'
 

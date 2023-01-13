281a9878e789cbb758748fa5e3f318c4e481f175
erickarroyo1 <earroyo@lulobank.com>
2023-01-12 21:30:58 -0500

    bootcamp/terraform/challenge-1-1

--- Fase4-Terraform/challenge1-tf/ec2.tf
+++ Fase4-Terraform/challenge1-tf/ec2.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "bootcamp-servers" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = data.aws_subnet.az_a.id
-  #subnet_id              = data.aws_subnet.az_a.id
   vpc_security_group_ids = [aws_security_group.grupo_seguridad.id]
   tags                   = local.common_tags
   user_data              = file("${var.bootstrap-bootcamp-server}")

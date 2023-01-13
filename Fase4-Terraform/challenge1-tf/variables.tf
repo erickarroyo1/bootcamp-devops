4cecc068b567949c0a21fb387183f6d78037467e
erickarroyo1 <earroyo@lulobank.com>
2023-01-12 21:25:53 -0500

    bootcamp/terraform/challenge-1

---
+++ Fase4-Terraform/challenge1-tf/variables.tf
@@ -0,0 +1,60 @@
+variable "access_key" {
+  type    = string
+  default = "AKIAWMQQFKAMQ7DIXF6N"
+}
+
+variable "secret_key" {
+  type    = string
+  default = "4NcucSDTOYU2Z3CbOpxntuDlBGv2cbfeLH9khn8P"
+}
+
+variable "token" {
+  type    = string
+  default = ""
+}
+
+variable "app" {
+  type    = string
+  default = "nginx"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0fe472d8a85bc7b0e"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "owner" {
+  type        = string
+  description = "owner of deployer's devops"
+  default     = "Erick Arroyo"
+}
+
+variable "project" {
+  type        = string
+  description = "project name"
+  default     = "challenge-1-tf"
+}
+
+variable "bootcamp" {
+  type        = string
+  description = "bootcamp for devops engineer"
+  default     = "bootcamp-devops"
+}
+
+variable "instance_count" {
+  type        = number
+  description = "the number of instances"
+  default     = 2
+}
+
+
+variable "bootstrap-bootcamp-server" {
+  type        = string
+  description = "path to bootstrap script - user-data"
+  default     = "./bootstrap/bootstrap-server.sh"
+}
